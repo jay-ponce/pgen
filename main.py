1a7ee0312a21031cc32d519c06aa68dfbac02289
Jay Ponce <jay@jayponce.com>
2025-05-23 22:13:12 -0500

    Updated to accept args, if not then ask for input

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
-from src.gen import Generator
 import sys
+from src.gen import Generator
+
+
+def ask_yes_no(prompt):
+    return input(prompt + " (y/n): ").strip().lower() == 'y'
+
+
+def ask_length(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a valid integer.")
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python main.py <username_length> <password_length> <pin_length>")
-        sys.exit(1)
-
-    # Read arguments from the command line
-    userlen = int(sys.argv[1])
-    passlen = int(sys.argv[2])
-    pinlen = int(sys.argv[3])
-
+    args = sys.argv[1:]
     gen = Generator()
 
-    # userlen = input("How long do you want your username: ")
-    # passlen = input("How long do you want your password: ")
-    # pinlen = input("How long do you want your PIN: ")
-    # strict_mode = input("Use strict PIN mode? (y/n): ").lower() == 'y'
+    userlen = None
+    passlen = None
+    pinlen = None
 
-    print("\nGenerated F.Username: " + gen.generate_username(userlen))
-    print("Generated human readbale username: " +
-          gen.generate_human_readable_username(userlen))
-    print("Generated Password: " + gen.generate_password(passlen))
-    print("Generated PIN: " + gen.generate_pin(pinlen))
+    generate_user = False
+    generate_pass = False
+    generate_pin = False
+
+    # Parse arguments if provided. If an argument is provided, it will only generate that argument.
+    for arg in args:
+        if arg.startswith("userlen="):
+            userlen = int(arg.split("=")[1])
+            generate_user = True
+        elif arg.startswith("passlen="):
+            passlen = int(arg.split("=")[1])
+            generate_pass = True
+        elif arg.startswith("pinlen="):
+            pinlen = int(arg.split("=")[1])
+            generate_pin = True
+
+    # If no arguments are provided, ask the user for input
+    if not args:
+        generate_user = ask_yes_no("Do you want to generate a username?")
+        generate_pass = ask_yes_no("Do you want to generate a password?")
+        generate_pin = ask_yes_no("Do you want to generate a PIN?")
+
+    # Ask for lengths only if needed
+    if generate_user and userlen is None:
+        userlen = ask_length("How long do you want your username: ")
+    if generate_pass and passlen is None:
+        passlen = ask_length("How long do you want your password: ")
+    if generate_pin and pinlen is None:
+        pinlen = ask_length("How long do you want your PIN: ")
+
+    # Perform generation at the end
+    print("\n--- Generated Output ---")
+    if generate_user:
+        print("Generated F.Username: " + gen.generate_username(userlen))
+        print("Generated human-readable username: " +
+              gen.generate_human_readable_username(userlen))
+    if generate_pass:
+        print("Generated Password: " + gen.generate_password(passlen))
+    if generate_pin:
+        print("Generated PIN: " + gen.generate_pin(pinlen))
